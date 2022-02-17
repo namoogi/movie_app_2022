cf147ec64ab7b28c766698eb7a703b74a6f1054c
유남욱 <namoogi@yunam-ug-ui-MacBookAir.local>
2022-02-17 16:59:11 +0900

    Updates

---
+++ precache-manifest.c75bb5caafc928b3ca42484dc1fdc4cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4162fe2e8519b593127693d0058e07",
+    "url": "/namoogi/movie_app_2022/index.html"
+  },
+  {
+    "revision": "7127a6b192389252e841",
+    "url": "/namoogi/movie_app_2022/static/css/main.eec1359d.chunk.css"
+  },
+  {
+    "revision": "dccd5b535b3072b59976",
+    "url": "/namoogi/movie_app_2022/static/js/2.bab2802a.chunk.js"
+  },
+  {
+    "revision": "7127a6b192389252e841",
+    "url": "/namoogi/movie_app_2022/static/js/main.da54f168.chunk.js"
+  },
+  {
+    "revision": "89a7ae3b6beee97b2dff",
+    "url": "/namoogi/movie_app_2022/static/js/runtime~main.a45548dc.js"
+  }
+]);
